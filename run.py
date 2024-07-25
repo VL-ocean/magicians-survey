2fcdb568dc57d1fed5429c022b00485fad2d9286
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-07-25 11:59:05 +0000

    Add function description, fix indentation

--- run.py
+++ run.py
@@ -13,21 +13,23 @@ from rich.table import Table
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('survey-data')
-SHEET_DATA = SHEET.worksheet('data')
-SHEET_QUESTIONS = SHEET.worksheet('questions')
+SHEET = GSPREAD_CLIENT.open("survey-data")
+SHEET_DATA = SHEET.worksheet("data")
+SHEET_QUESTIONS = SHEET.worksheet("questions")
 
 
-# Clear the terminal from all text
+# source: https://github.com/DennisSchenkel/PP3
 def wipe_terminal():
-    # Delete all text in the terminal
+    """
+    Clears the terminal from all text
+    """
     if os.name == "posix":  # Identify if OS is macOS or Linux
         os.system("clear")
     elif os.name == "nt":  # Identify of OS is Windows
@@ -35,11 +37,11 @@ def wipe_terminal():
 
 
 def get_questons():
-
-    # read the quantity of questions in the sheet
+    """
+    Gets data from questions worksheet
+    and return them for next function
+    """
     questions_count = len(SHEET_QUESTIONS.col_values(1))
-
-    # prepare new list for questions
     questions_list = []
 
     for index in range(1, questions_count + 1):
@@ -51,7 +53,10 @@ def get_questons():
 
 
 def get_date_and_time():
-
+    """
+    Gets current date and time in readable format,
+    return them in a list
+    """
     user_date = time.strftime("%d %b %Y", time.gmtime())
     user_time = time.strftime("%H:%M:%S", time.gmtime())
 
@@ -59,7 +64,11 @@ def get_date_and_time():
 
 
 def display_questions(questions):
-
+    """
+    Reads data from the questions list,
+    prints them out for the user one by one,
+    takes user`s answer and saves it in a list
+    """
     answers_list = get_date_and_time()
 
     for question_list in questions:
@@ -69,17 +78,26 @@ def display_questions(questions):
         question = "\n" + question_list[0] + "\n"
         print(question)
 
-        # loop through answers and print each of them adding the option's number
-        for index in (range(len(question_list[1]))):
+        # loop through answers
+        # and print each of them adding the option's number
+        for index in range(len(question_list[1])):
             num = index + 1
-            option_for_user = f"({Fore.YELLOW}{num}{Style.RESET_ALL}) - {question_list[1][index]}"
+            option_for_user = (
+                f"({Fore.YELLOW}{num}{Style.RESET_ALL})"
+                f" - {question_list[1][index]}")
             print(option_for_user)
 
         while True:
             try:
                 # get user's input
                 print("")
-                answer = int(input(Fore.YELLOW + "Enter your option number here: " + Style.RESET_ALL))
+                answer = int(
+                    input(
+                        Fore.YELLOW
+                        + "Enter your option number here: "
+                        + Style.RESET_ALL
+                    )
+                )
                 if answer == 0:
                     wipe_terminal()
                     raise SystemExit
@@ -94,27 +112,73 @@ def display_questions(questions):
             except ValueError:
                 print(Fore.RED + "Please enter a number" + Style.RESET_ALL)
             except IndexError:
-                print(Fore.RED + "Please choose a number from available options" + Style.RESET_ALL)
+                print(
+                    Fore.RED
+                    + "Please choose a number from available options"
+                    + Style.RESET_ALL
+                )
 
-    return answers_list 
+    return answers_list
 
 
 def push_user_data(data):
+    """
+    Transfers user's answers into the worksheet
+    """
     SHEET_DATA.append_row(data)
-    print(Fore.BLUE + "\nThe answers has been saved.\nThank you for filling out our survey!" + Style.RESET_ALL)
+    print(
+        Fore.BLUE
+        + "\nThe answers has been saved."
+        + "\nThank you for filling out our survey!"
+        + Style.RESET_ALL
+    )
     time.sleep(2)
 
 
 def choice():
-
-    print(Fore.GREEN + "\n\nWelcome to the anonymous survey for magicians!\n" + Style.RESET_ALL)
+    """
+    Prints out the welcome screen to the user with some info,
+    asks to start the survey
+    """
+    print(
+        Fore.GREEN
+        + "\n\nWelcome to the anonymous survey for magicians!\n"
+        + Style.RESET_ALL
+    )
     print(Fore.BLUE + "You will go through 10 questions." + Style.RESET_ALL)
-    print(Fore.BLUE + "Please choose the option that suits you the most." + Style.RESET_ALL)
-    print(Fore.BLUE + "If you would like to exit the survey, please enter " + Style.RESET_ALL + Fore.RED + "0" + Style.RESET_ALL + Fore.BLUE + " anytime you feel so.\n" + Style.RESET_ALL)
+    print(
+        Fore.BLUE
+        + "Please choose the option that suits you the most."
+        + Style.RESET_ALL
+    )
+    print(
+        Fore.BLUE
+        + "If you would like to exit the survey, please enter "
+        + Style.RESET_ALL
+        + Fore.RED
+        + "0"
+        + Style.RESET_ALL
+        + Fore.BLUE
+        + " anytime you feel so.\n"
+        + Style.RESET_ALL
+    )
+
     while True:
         try:
             print("Would you like to start the survey?")
-            option = int(input("(" + Fore.YELLOW + "1" + Style.RESET_ALL + ") - yes or (" + Fore.YELLOW + "2" + Style.RESET_ALL + ") - no: "))
+            option = int(
+                input(
+                    "("
+                    + Fore.YELLOW
+                    + "1"
+                    + Style.RESET_ALL
+                    + ") - yes or ("
+                    + Fore.YELLOW
+                    + "2"
+                    + Style.RESET_ALL
+                    + ") - no: "
+                )
+            )
             if option == 0:
                 wipe_terminal()
                 raise SystemExit
@@ -125,12 +189,20 @@ def choice():
                 return True
                 break
             else:
-                print(Fore.RED + "Please choose a number from available options" + Style.RESET_ALL)
+                print(
+                    Fore.RED
+                    + "Please choose a number from available options"
+                    + Style.RESET_ALL
+                )
         except ValueError:
             print(Fore.RED + "Please enter a number" + Style.RESET_ALL)
 
 
 def count_keyword(keyword, data_list):
+    """
+    Counts how many of the keyword (answer option) is
+    in provided list (list of all user answers for one question)
+    """
     num = 0
     for list in data_list:
         if keyword in list:
@@ -139,6 +211,10 @@ def count_keyword(keyword, data_list):
 
 
 def count_percentage(num_of_keyword, num_of_replies):
+    """
+    Counts percentage of one answer option compared to
+    total number of answers for one question
+    """
     if num_of_keyword == 0:
         return 0
     else:
@@ -147,13 +223,14 @@ def count_percentage(num_of_keyword, num_of_replies):
 
 
 def build_statistics(questions):
-
+    """
+    Controls several functions that build statistics list,
+    calls them for each question and answer option from the survey
+    """
     statistics_list = []
-    
     list_of_lists = SHEET_DATA.get_all_values()
     data_list = list_of_lists[1:]
     num_of_replies = len(data_list)
-
     statistics_list.append(num_of_replies)
 
     for question in questions:
@@ -162,7 +239,9 @@ def build_statistics(questions):
         for keyword in question[1]:
 
             num_of_keyword = count_keyword(keyword, data_list)
-            percent_of_keyword = count_percentage(num_of_keyword, num_of_replies)
+            percent_of_keyword = count_percentage(
+                num_of_keyword, num_of_replies
+            )
             question_statistics.append(percent_of_keyword)
 
         statistics_list.append(question_statistics)
@@ -171,9 +250,15 @@ def build_statistics(questions):
 
 
 def show_statistics(questions, statistics):
-
+    """
+    Builds a table to display the user overall percentage
+    calculated based on the total number of replies
+    """
     wipe_terminal()
-    print(f"The survey was completed by {Fore.CYAN}{statistics[0]}{Style.RESET_ALL} people.\nYou can see the overall statistics below")
+    print((
+        f"The survey was completed by {Fore.CYAN}{statistics[0]}"
+        f"{Style.RESET_ALL} people.\nYou can see the overall statistics below")
+    )
     print("-" * 79)
     print("")
     time.sleep(3)
@@ -183,8 +268,10 @@ def show_statistics(questions, statistics):
         table = Table(title=question[0], title_style="cyan")
 
         table.add_column("Option", style="magenta", no_wrap=True)
-        table.add_column("Percentage", style="green",  justify="right", no_wrap=True)
-    
+        table.add_column(
+            "Percentage", style="green", justify="right", no_wrap=True
+        )
+
         for option, percent in zip(question[1], stats):
             table.add_row(option, str(percent) + " %")
 
@@ -195,11 +282,25 @@ def show_statistics(questions, statistics):
 
 
 def exit_or_restart():
-
+    """
+    Offers the user an option to complete the survey again or exit the app
+    """
     while True:
         try:
             print("\nWould you like to start the survey again or exit?")
-            option = int(input("(" + Fore.YELLOW + "1" + Style.RESET_ALL + ") - survey or (" + Fore.YELLOW + "2" + Style.RESET_ALL + ") - exit: "))
+            option = int(
+                input(
+                    "("
+                    + Fore.YELLOW
+                    + "1"
+                    + Style.RESET_ALL
+                    + ") - survey or ("
+                    + Fore.YELLOW
+                    + "2"
+                    + Style.RESET_ALL
+                    + ") - exit: "
+                )
+            )
             if option == 0:
                 wipe_terminal()
                 raise SystemExit
@@ -210,13 +311,19 @@ def exit_or_restart():
                 return True
                 break
             else:
-                print(Fore.RED + "Please choose a number from available options" + Style.RESET_ALL)
+                print(
+                    Fore.RED
+                    + "Please choose a number from available options"
+                    + Style.RESET_ALL
+                )
         except ValueError:
             print(Fore.RED + "Please enter a number" + Style.RESET_ALL)
 
 
 def main():
-    
+    """
+    Controls the app flow
+    """
     questions = get_questons()
     user_answer = display_questions(questions)
     push_user_data(user_answer)
@@ -226,15 +333,6 @@ def main():
         main()
 
 
+# Starts the survey, offers the exit option
 if choice():
     main()
-
-
-
-
-
-
-
-
-
-
